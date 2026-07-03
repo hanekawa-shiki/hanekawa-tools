c6283f60fbb33e82f49275c15823970d7d7ca786
shiki <kamiacgxu@gmail.com>
2026-07-03 14:46:30 +0800

    chore: change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,39 @@
 import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import htmlBuildTime from './vite-plugins/htmlBuildTime';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-    nodePolyfills({
-      include: ['path'],
-    }),
-    htmlBuildTime(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+  const envDir = path.resolve(__dirname, 'env');
+  const env = loadEnv(mode, envDir, '');
+
+  return {
+    envDir: 'env',
+    plugins: [
+      tailwindcss(),
+      react(),
+      nodePolyfills({
+        include: ['path'],
+      }),
+      htmlBuildTime(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    port: 9090,
-    open: true,
-  },
+    server: {
+      port: 9090,
+      open: true,
+    },
+    base: env.VITE_BASE_PATH || '/',
+    build: {
+      outDir: env.VITE_OUT_DIR || 'dist',
+    },
+  };
 });
