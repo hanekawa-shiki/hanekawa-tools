da4264ef0fda74cc847871d86dfdd7e2ea9ddb1f
shiki <kamiacgxu@gmail.com>
2026-07-08 09:04:08 +0800

    fix: env config

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,5 +2,5 @@ import { createApi } from './request';
 
 export const fetchHolidaysApi = createApi<ApiResponse>({
   method: 'POST',
-  url: 'https://holiday-cn-worker.angelbeast.workers.dev/api/holidays/year',
+  url: '/holidays/year',
 });
